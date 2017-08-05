8f6efb204fdd41a0bb45a3458d48e25c0c3a7b97
Jimmy LERAY <jimmy.leray@zenika.com>
2017-08-06 00:34:27 +0200

    Main title change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-		<title>Cities Online Alpha</title>
+		<title>Cities Online</title>
 		<link rel="icon" href="/images/site-icon.png" type="image/png">
 		<meta name="description" content="Jeu multijoueur de simulation urbaine gratuit, dans lequel vous incarnez le maire d'une ville en définissant sa politique.">
 		<meta name="keywords" content="jeu vidéo, simulation urbaine, gratuit, online, multijoueur, amis, cities, city builder, politique, maire">
@@ -10,15 +10,15 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	</head>
-	
+
 	<body>
 		<div id="main_container">
-			<div id="game_container">	
+			<div id="game_container">
 				<div id="game_map">
 					<div id="map_container" class="map_global map_elements"></div>
 				</div>
 			</div>
-			
+
 			<div id="interface_container">
 				<div id="interface_tool_tree" onclick="changeTool('tree')"><img src="./images/icon_tree.png" title="Ajoute des arbres" alt="icone,tree" width="30" height="30"></div>
 				<div id="interface_tool_road" onclick="changeTool('road')"><img src="./images/icon_road.png" title="Construit des routes" alt="icone,route" width="30" height="30"></div>
@@ -28,12 +28,12 @@
 				<div id="interface_tool_industry" onclick="changeTool('industry')"><img src="./images/icon_industry.png" title="Délimite une zone industrielle" alt="icone,industry" width="30" height="30"></div>
 				<div id="interface_tool_delete" onclick="changeTool('delete')"><img src="./images/icon_delete.png" title="Détruit des éléments" alt="icone,delete" width="30" height="30"></div>
 			</div>
-			
+
 			<div id="interface_minimap">
 				<canvas id="minimap_background"></canvas>
 				<canvas id="minimap_selector"></canvas>
 			</div>
-			
+
 			<div id="ressources_bar">
 				<div id="ressources_level">
 					<img src="./images/icon_star_blue.png" alt="fullscreen" width="20" height="20">
@@ -53,7 +53,7 @@
 					<span id="rubis_current">0</span>
 				</div>-->
 			</div>
-			
+
 			<div id="menu_bar">
 				<div id="fullscreen_icon" onclick="changeScreenMode(document.body)">
 					<img src="./images/icon_fullscreen.png" title="Utilisez F11 pour basculer en mode plein écran" alt="fullscreen" width="23" height="19">
@@ -65,7 +65,7 @@
 					<img src="./images/audio_volume_3.png" id="volume_level_img" title="Cliquez pour ajuster le volume du jeu" alt="volume" width="25" height="20">
 				</div>
 			</div>
-			
+
 			<div id="options_menu">
 				<span>Réglages à venir</span>
 				<!--<form>
@@ -73,17 +73,17 @@
 				</form>-->
 			</div>
 		</div>
-		
+
 		<div id="preload_img" style="display:none;">
 			<img src="./images/tree0.png" class="preload_img_tree" id="preload_img_tree-0-21-45" alt="tree" width="40" height="53">
 			<img src="./images/tree0grey.png" class="preload_img_tree" id="preload_img_tree_grey-0-21-45" alt="tree" width="40" height="53">
 			<img src="./images/tree0red.png" class="preload_img_tree" id="preload_img_tree_red-0-21-45" alt="tree" width="40" height="53">
-		
+
 			<img src="./images/roads/road.png" class="preload_img_road" id="preload_img_road" alt="road" width="90" height="1600">
 			<img src="./images/roads/road_grey.png" class="preload_img_road" id="preload_img_road_grey" alt="road" width="90" height="1600">
 			<img src="./images/roads/road_red.png" class="preload_img_road" id="preload_img_road_red" alt="road" width="90" height="1600">
 		</div>
-		
+
 		<div id="audio_lecteur"></div>
 
 		<script type="text/javascript" src="./scripts.js"></script>
